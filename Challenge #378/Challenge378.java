04a1b6a0901070add106dcc0474e2c1c2fc1962f
AdrianKluba <53834756+AdrianKluba@users.noreply.github.com>
2019-08-29 13:07:26 +0200

    Update Challenge378.java

--- Challenge #378/Challenge378.java
+++ Challenge #378/Challenge378.java
@@ -12,60 +12,42 @@ If N is greater than the length of this new sequence (i.e. warmup3), stop and re
 Subtract 1 from each of the first N elements of the new sequence (i.e. warmup4).
 
 */
-package challenge.pkg378;
 
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
-
-
-/**
- *
- * @author Horikss
- */
 public class Challenge378 {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-         long number2 = 0;
-         int numberL = 0;
-        
-        do{
-           try{
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Type number");
-        number2 = scanner.nextInt();
-      }catch(InputMismatchException n){
-          System.out.println("Type only numbers");
-      }
-      }
-      while(number2 == 0 );
-       
-    number hakimi = new number();
+	/**
+	 * @param args the command line arguments
+	 */
+	public static void main(String[] args) {
+		long userNumber = 0;
 
+		do {
+			try {
+				Scanner scanner = new Scanner(System.in);
+				System.out.println("Type number");
+				userNumber = scanner.nextInt();
+			} catch (InputMismatchException n) {
+				System.out.println("Type only numbers");
+			}
+		} while (userNumber == 0);
+		// Havel-Hakimi algorithm
+		Number hakimi = new Number();
 
-    hakimi.putList(number2);
-    
-    hakimi.Option1();
-        System.out.println("Option1 Remove all 0's from the sequence, ");
-    hakimi.showList();
-        System.out.println("Option2");
-        hakimi.Option2();
-        hakimi.showList();
-        System.out.println("Option 3");
-        hakimi.Option3();
-        System.out.println("Option 4");
-        hakimi.Option4();
-        
+		hakimi.putList(userNumber);
 
-        
-  
-    
+		hakimi.removeZero();
+		System.out.println("Remove all 0's from the sequence, ");
+		hakimi.showList();
+		System.out.println("Option2");
+		hakimi.sortArray();
+		hakimi.showList();
+		System.out.println("Option 3");
+		hakimi.checkLengthOfArray();
+		System.out.println("Option 4");
+		hakimi.Result();
 
-
-
-    }
+	}
 }
-
