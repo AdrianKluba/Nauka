d0500cb8e8cd5a703c245eaf73ad9ac4f000efbb
AdrianKluba <53834756+AdrianKluba@users.noreply.github.com>
2019-08-29 12:37:02 +0200

    Update Book.java

--- library/Book.java
+++ library/Book.java
@@ -9,30 +9,32 @@ package Library;
  *
  * @author Horikss
  */
-public class Book{
-    Autor Autor;
-    Description Description;
-    int Price;
- 
-    public Book (Autor Autor, Description Description, int Price){
-        this.Autor = Autor;
-        this.Description = Description;
-        this.Price = Price;
-    }
+public class Book {
+	Autor Autor;
+	Description Description;
+	int Price;
 
+	public Book(Autor Autor, Description Description, int Price) {
+		this.Autor = Autor;
+		this.Description = Description;
+		this.Price = Price;
+	}
 
-     public String getAutor(String Autor){
-        return Autor;
-    }
-     public String getDescription(String Description){
-        return Description;
-    }
-      public int getPrice(int Price){
-         return Price;
-     }
-    @Override
-    public String toString(){
-         return "Autor: "+ Autor + " Description: " + Description + " Price: "+ Price;
-     }
+	public String getAutor(String Autor) {
+		return Autor;
+	}
+
+	public String getDescription(String Description) {
+		return Description;
+	}
+
+	public int getPrice(int Price) {
+		return Price;
+	}
+
+	@Override
+	public String toString() {
+		return "Autor: " + Autor + " Description: " + Description + " Price: " + Price;
+	}
 
 }
