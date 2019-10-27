f7d26ee9114d84fc1442b89b967ae1dab61adf30
AdrianKluba <53834756+AdrianKluba@users.noreply.github.com>
2019-10-27 19:39:00 +0100

    Update Monster.java

--- Extends and Implements/Monster.java
+++ Extends and Implements/Monster.java
@@ -4,7 +4,7 @@ package pkgextends.and.pkgimplements;
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
- */
+ */ 
 
 /**
  *
