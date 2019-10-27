4fd7762f5fee601852875dc66474b29b0ecc115f
AdrianKluba <53834756+AdrianKluba@users.noreply.github.com>
2019-10-27 19:34:10 +0100

    Update [Reddit] Challenge377.java

--- [Reddit] Challenge377.java
+++ [Reddit] Challenge377.java
@@ -2,7 +2,7 @@
 for instance, if the crate is size X = 25 by Y = 18, and the boxes are size x = 6 by y = 5, then the answer is 12. 
 You can fit 4 boxes along the x-axis (because 6*4 <= 25), and 3 boxes along the y-axis (because 5*3 <= 18), so in total you can fit 4*3 = 12 boxes in a rectangle.
 BONUS ou now have the option of rotating all boxes by 90 degrees, so that you can treat a set of 6-by-5 boxes as a set of 5-by-6 boxes. 
-You do not have the option of rotating some of the boxes but not others.
+You do not have the option of rotating some of the boxes but not others. 
 */
 
 
