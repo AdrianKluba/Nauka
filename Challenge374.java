d412dc2c88b56b8797da96797da683c5d5060776
AdrianKluba <53834756+AdrianKluba@users.noreply.github.com>
2020-02-26 15:12:29 +0100

    Update Challenge374.java

--- Challenge374.java
+++ Challenge374.java
@@ -15,7 +15,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.InputMismatchException;
 import java.util.List;
-import java.util.Scanner;
+import java.util.Scanner; 
 
 public class Challenge370 {
 
